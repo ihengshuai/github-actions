599d7f70c21a04ab2738498ed34eb3f45de91b10
devin <devin@qoo-app.com>
2022-09-27 12:39:55 +0800

    update github-page ci

--- scripts/deploy-page.sh
+++ scripts/deploy-page.sh
@@ -4,15 +4,15 @@ remote=$(git config remote.origin.url)
 
 echo $remote
 
-mkdir gh-pages-branch
-cd gh-pages-branch
+mkdir ../gh-pages-branch
+cd ../gh-pages-branch
 git init
 git remote add --fetch origin "$remote"
 git checkout -b gh-pages
 
 echo $BUNDLE_DIST
 
-cp -a ../$BUNDLE_DIST/* .
+cp -a ./github-actions/$BUNDLE_DIST/* .
 git config --global user.email "wsm_1105@163.com"
 git config --global user.name "ihengshuai"
 git add . -A
