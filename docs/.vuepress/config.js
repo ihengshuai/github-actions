3643dc908d0d2f1a81037dd57a96ba387a25ec1b
devin <devin@qoo-app.com>
2022-09-27 12:00:47 +0800

    update github-page ci

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'en-US',
+  base: "/github-actions/",
   title: 'Hello VuePress',
   description: 'Just playing around',
 })
