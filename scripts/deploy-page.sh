982602add4196c1a52309f4f775226f38fd10ea4
devin <devin@qoo-app.com>
2022-09-27 14:37:56 +0800

    update github-page ci

--- scripts/deploy-page.sh
+++ scripts/deploy-page.sh
@@ -1,27 +1,29 @@
+#!/usr/bin/env sh
+
 set -e
 
-remote="$(git config remote.origin.url)"
+echo $ACCESS_TOKEN
+
+remote=$(git config remote.origin.url)
 
 echo $remote
-pwd
-cd ..
+
 mkdir gh-pages-branch
-ls
 cd gh-pages-branch
 git init
-git remote add origin "$remote"
+githubUrl="https://ihengshuai:${GITHUB_TOKEN}@github.com/ihengshuai/github-actions.git"
+git remote add --fetch origin "$remote"
 git checkout -b gh-pages
 
 echo $BUNDLE_DIST
 
-cp -a ../github-actions/$BUNDLE_DIST/* .
-githubUrl="git@github.com:ihengshuai/github-actions"
+cp -a ../$BUNDLE_DIST/* .
 git config --global user.email "wsm_1105@163.com"
 git config --global user.name "ihengshuai"
 git add . -A
 git commit -m 'update page'
-git push origin gh-pages
-git push -f $githubUrl master:gh-pages
+# git push -f origin -q gh-pages
+git push -f $githubUrl -q master:gh-pages
 cd ..
 rm -rf gh-pages-branch
 echo deploy successfully
