ad0ddc25df21a7992ad36bc57d2045f7680d72f8
Peachick <peachick@aliyun.com>
2022-09-28 08:37:15 +0800

    fix(test): jest test is fulled (#15)

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,12 +1,3 @@
 test("test demo", () => {
     expect(3).toBe(3)
-    expect(2).toBe(2)
-    expect(4).toBe(4)
-    expect(5).toBe(5)
-    // this is a error and will being fix
-    expect(6).toBe(6)
-    expect(7).toBe(7)
-    expect(8).toBe(8)
-    expect(9).toBe(9)
-    expect(10).toBe(10)
 })
