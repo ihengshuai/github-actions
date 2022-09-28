43cd28d48ba00a251797919c9edd0557adbfa8da
lucky-user <devin@qoo-app.com>
2022-09-28 08:09:13 +0800

    lucky-user fix a error jest test

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -3,6 +3,6 @@ test("test demo", () => {
     expect(2).toBe(2)
     expect(4).toBe(4)
     expect(5).toBe(5)
-    // this is a error
-    expect(6).toBe(7)
+    // this is a error and will being fix
+    expect(6).toBe(6)
 })
